f1f3fc7b523c02b264b5cbcba722de815e4ba4a8
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 01:24:41 +0530

    Added: Calculate Wage For Month

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -2,37 +2,37 @@ package com.bridgelabz.employeewagecompute;
 
 import java.util.Random;
 public class EmployeeWageComputation {
-public static final int IS_FULL_TIME = 1;
-public static final int IS_PART_TIME = 2;
-public static final int WAGE_PER_HR=20;
-int empCheck=0;
-int empHrs=0;
-int empWage=0;
-    public void getAttendance() {
-	Random r = new Random();
-	empCheck = r.nextInt(3);
-    }
-    public void employeeCheck() {
-    	switch(empCheck) {
-	case IS_FULL_TIME:
-	    empHrs = 8;
-	    System.out.println("Employee is Full Time");
-	    break;
-	case IS_PART_TIME:
-	    empHrs = 4;
-	    System.out.println("Employee is Part Time");
-	    break;
-	default:
-	    empHrs = 0;
-	    System.out.println("Employee is Absent");
-	    break;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HR=20;
+	public static final int NUM_WORKING_DAYS=20;
+	int empCheck=0;
+	int empHrs=0;
+	int totalEmpWage=0;
+	int empWage=0;
+        public void employeeCheck() {
+	    for (int i = 0; i <= NUM_WORKING_DAYS; i++) {
+	        Random random = new Random();
+	        empCheck = random.nextInt(3);
+	        switch (empCheck) {
+    	        case IS_FULL_TIME:
+    	            empHrs = 8;
+    	 	    break;
+    	        case IS_PART_TIME:
+    	            empHrs = 4;
+    	            break;
+    	        default:
+    	            empHrs = 0;
+    	            break;
+    	     }
+	     empWage = WAGE_PER_HR * empHrs;
+    	     totalEmpWage += empWage ;
+    	     System.out.println("Daily wage of the employee: " + empWage);
 	}
-	empWage = empHrs * WAGE_PER_HR;
-	System.out.println("Daily Wage of Employee is: " + empWage);
-    }
-    public static void main(String[] args) {
-    	EmployeeWageComputation empwage = new EmployeeWageComputation();
-    	empwage.getAttendance();
-    	empwage.employeeCheck();
-    }
+	System.out.println("Total Employee Wage for a Month is: " + totalEmpWage );
+	}
+	public static void main(String[] args) {
+		EmployeeWageComputation empwage = new EmployeeWageComputation();
+		empwage.employeeCheck();
+	}
 }
