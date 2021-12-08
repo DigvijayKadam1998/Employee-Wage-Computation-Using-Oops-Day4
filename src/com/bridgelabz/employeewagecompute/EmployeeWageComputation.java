0f8e5368741b499798c90fad1817c1044ee25e20
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 01:16:04 +0530

    Added: Calculate Part Time Employee Wage using Switch Case Statement

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -2,29 +2,32 @@ package com.bridgelabz.employeewagecompute;
 
 import java.util.Random;
 public class EmployeeWageComputation {
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int WAGE_PER_HR=20;
-    int empCheck=0;
-    int empHrs=0;
-    int empWage=0;
+public static final int IS_FULL_TIME = 1;
+public static final int IS_PART_TIME = 2;
+public static final int WAGE_PER_HR=20;
+int empCheck=0;
+int empHrs=0;
+int empWage=0;
     public void getAttendance() {
 	Random r = new Random();
 	empCheck = r.nextInt(3);
     }
     public void employeeCheck() {
-    	if(empCheck==IS_FULL_TIME){
-	    empHrs= 8;
+    	switch(empCheck) {
+	case IS_FULL_TIME:
+	    empHrs = 8;
 	    System.out.println("Employee is Full Time");
-	}
-	else if(empCheck==IS_PART_TIME) {
+	    break;
+	case IS_PART_TIME:
 	    empHrs = 4;
 	    System.out.println("Employee is Part Time");
-	}
-	else {
+	    break;
+	default:
+	    empHrs = 0;
 	    System.out.println("Employee is Absent");
+	    break;
 	}
-	empWage= WAGE_PER_HR * empHrs;
+	empWage = empHrs * WAGE_PER_HR;
 	System.out.println("Daily Wage of Employee is: " + empWage);
     }
     public static void main(String[] args) {
