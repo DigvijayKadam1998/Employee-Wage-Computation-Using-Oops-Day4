03f4d7d54b3782ce838819cc70df8939880a2601
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 00:53:36 +0530

    Added: Checking Employee is Present or Absent

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.employeewagecompute;
 
+import java.util.Random;
 public class EmployeeWageComputation {
+    int IS_FULL_TIME = 1;
+    int empcheck=0;
+    public void getAttendance() {
+	Random r = new Random();
+	empcheck = r.nextInt(2);
+    }
+    public void employeeCheck() {
+	if(empcheck==IS_FULL_TIME) {
+	    System.out.println("Employee is Present");
+	}
+	else {
+	    System.out.println("Employee is Absent");
+	}
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Problem");
+    	EmployeeWageComputation empwage = new EmployeeWageComputation();
+    	empwage.getAttendance();
+    	empwage.employeeCheck();
     }
 }
