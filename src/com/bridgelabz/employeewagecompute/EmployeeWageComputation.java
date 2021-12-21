34327700efd60dce82c275b8e4ed53bf6ae9c4b9
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-21 10:29:01 +0530

    Added: Calculate Employee Wage For Multiple Companies

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -2,38 +2,40 @@ package com.bridgelabz.employeewagecompute;
 
 import java.util.Random;
 public class EmployeeWageComputation {
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static final int WAGE_PER_HR=20;
-    public static final int NUM_WORKING_DAYS=20;
-    public static final int MAX_HRS_IN_MONTH=100;
-    public void employeeCheck() {
-    int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage = 0, empCheck = 0;
-		
-    while (totalEmpHrs<= MAX_HRS_IN_MONTH  && totalWorkingDays < NUM_WORKING_DAYS) {
-          totalWorkingDays++;
-			
-    	  Random random = new Random();
-          empCheck = random.nextInt(3);
-			
-    	  switch (empCheck) {
-    	  case IS_FULL_TIME:
-	      empHrs =8;
-	      break;
-          case IS_PART_TIME:
-	      empHrs =4;
-	      break;
-          default:
-	      empHrs =0;
-          }
-          totalEmpHrs += empHrs;
-          System.out.println("Day:" + totalWorkingDays + " Emp Hr:" + empHrs + "" );
-    }
-    totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-    System.out.println("Total Emp Wage:" + totalEmpWage);
-    }
-    public static void main(String[] args) {
-    	EmployeeWageComputation empwage = new EmployeeWageComputation();
-    	empwage.employeeCheck();
-    }
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	
+	public static int computeEmpWage(String company,int empRatePerHour,
+			int numOfWorkingDays,int maxHoursPerMonth){
+	    	
+		//variables
+		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+	        
+		//Computation
+		while (totalEmpHrs<= maxHoursPerMonth  &&
+				totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck =(int) Math.floor(Math.random() * 10 ) % 3 ;
+			switch (empCheck) {
+			case  IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" + totalWorkingDays + " Emp Hr:" +empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage For Company:" +company+ " is:"+ totalEmpWage);
+		return totalEmpWage;
+
+	}
+	public static void main(String[] args){
+		computeEmpWage("Dmart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
+	}
 }
